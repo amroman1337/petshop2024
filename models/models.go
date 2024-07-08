a27460945e1acb336a4a960158f4ad841efdd5e1
amroman1337 <roma18705@ya.ru>
2024-07-08 14:44:50 +0300

    praktika

--- models/models.go
+++ models/models.go
@@ -4,16 +4,16 @@ import "gorm.io/gorm"
 
 type Pet struct {
 	gorm.Model
-	Type    string
-	Name    string
-	Age     int
-	Species string
-	Gender  string
-	Color   string
+	Type    string `json:"type" binding:"required"`
+	Name    string `json:"name" binding:"required"`
+	Age     int    `json:"age" binding:"required"`
+	Species string `json:"species" binding:"required"`
+	Gender  string `json:"gender" binding:"required"`
+	Color   string `json:"color" binding:"required"`
 }
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"unique"`
-	Password string
+	Username string `gorm:"unique" json:"Username" binding:"required"`
+	Password string `json:"Password" binding:"required"`
 }
